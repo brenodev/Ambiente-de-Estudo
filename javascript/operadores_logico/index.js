5fcc7f92fbe9681bc4a6b679265927fdddbbbdb2
brenodev <breno.souza.developer@gmail.com>
2019-08-11 00:39:43 -0300

    operadores lógicos

---
+++ javascript/operadores_logico/index.js
@@ -0,0 +1,21 @@
+var sexo = "M",
+  idade = 27;
+
+if (sexo === "M" && idade >= 18) {
+  console.log("Deve para entrada de 50$");
+}
+
+if (sexo === "F" || idade >= 18) {
+  console.log("entrada gratuita");
+}
+
+if (sexo !== "F" && 18) {
+  console.log("entra de graça");
+}
+// armazenar uma variavel
+
+var sexo = "M";
+
+var masculino = sexo === "M";
+
+console.log(masculino);
