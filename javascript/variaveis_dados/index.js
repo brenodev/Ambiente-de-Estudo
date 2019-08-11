9e57c77be124fa5fd8c64610b51f5f6544ed221d
brenodev <breno.souza.developer@gmail.com>
2019-08-10 21:03:56 -0300

    tipos de variaveis

---
+++ javascript/variaveis_dados/index.js
@@ -0,0 +1,18 @@
+// tipos de variáveis
+// String
+// Inteiro
+// Float
+// Boolean
+// Vetor
+// Objetos
+var nome = "Breno";
+var idade = 17;
+var peso = 85.0;
+var humano = true;
+var alunos = ["Breno", "João", "Maria"];
+var aluno = { nome: "Breno", idade: 27, peso: 85.0, humano: true };
+
+console.log(aluno);
+console.log(alunos);
+console.log(alunos[1]);
+console.log(aluno.nome);
