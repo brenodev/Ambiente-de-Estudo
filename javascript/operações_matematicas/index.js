bd2693122f42ace6cf3f124277b3843881a1a555
brenodev <breno.souza.developer@gmail.com>
2019-08-10 23:52:34 -0300

    operações matematicas

---
+++ javascript/operações_matematicas/index.js
@@ -0,0 +1,16 @@
+// observação, posso declarar uma variavel assim (var x = 10, y = 5)
+var x = 10;
+var y = 5;
+var resultado1 = x + y;
+var resultado2 = x - y;
+var resultado3 = x * y;
+var resultado4 = x / y;
+var resultado5 = x % y;
+
+console.log(resultado1, resultado2, resultado3, resultado4, resultado5);
+// para incrementar um valor por exemplo :
+
+// x = x + 3; // isso pode ser da seguinte forma
+x += 3;
+
+console.log(x);
