86764142877a1f1a672d3257aa2a48285c8ddfe7
brenodev <breno.souza.developer@gmail.com>
2019-08-11 01:04:38 -0300

    estrutura de repetição

---
+++ javascript/estrutura_de_repetição/index.js
@@ -0,0 +1,15 @@
+// for cria um intervalo
+
+// valor inicial ; condição; soma um valor a mais;
+for (var i = 0; i <= 100; i++) {
+  console.log(i);
+}
+
+var j = 0;
+while (j <= 100) {
+  console.log(j);
+  j++;
+}
+// quando usar for e quando usar while?
+// for sempre que souber o tamanho do intervalo
+// while é quando geralmente não sabemos quantas vezes o fluxo será executado
