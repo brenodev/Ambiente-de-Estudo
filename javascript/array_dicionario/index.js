5ce482fd4ff346bd4ae26d06016f93c4bc314b7b
brenodev <breno.souza.developer@gmail.com>
2019-08-17 12:17:31 -0300

    array e dicionario

---
+++ javascript/array_dicionario/index.js
@@ -0,0 +1,36 @@
+// definindo array
+var lista = ["maçã", "uva", "melancia", "banana"];
+//console.log(lista); // exibindo no console do browser a lista
+//alert(lista[1]); // exibindo um alerta na posição 1 que será uva
+//console.log(lista.length); // para ver o tamanho da lista
+//lista.push("amora"); // inclui mais 1 item na lista
+//lista.pop() // para excluir 1 item da lista
+//console.log(lista.reverse()); // usado para reverter a lista
+//console.log(lista.toString()); // retorna uma lista de string
+//console.log(lista.join(" - ")); // transforma a string
+
+// definindo dicionário
+// objeto fruta
+var fruta = {
+  nome: "maçã",
+  cor: "vermelha"
+};
+// console.log(fruta);
+// console.log(fruta.nome); // busca i objeto e exibe o nome da fruta no console
+
+// definindo um array de objetos
+var frutas = [
+  {
+    nome: "uva",
+    cor: "roxa"
+  },
+  {
+    nome: "maçã",
+    cor: "vermelha"
+  },
+  {
+    nome: "goiaba",
+    cor: "verde"
+  }
+];
+console.log(frutas);
