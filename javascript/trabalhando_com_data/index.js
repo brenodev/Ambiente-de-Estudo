dd419f5f21c6f7837c6f6cc32e786aba79dfe6d3
brenodev <breno.souza.developer@gmail.com>
2019-08-17 13:02:06 -0300

    trabalhando com data

---
+++ javascript/trabalhando_com_data/index.js
@@ -0,0 +1,8 @@
+var d = new Date();
+console.log(d); // exibe a data mes e ano
+
+var d2 = new Date();
+console.log(d2.getMonth() + 1); // exibe o mês, porém devo acrescentar +1 pois começa do 0
+
+var d3 = new Date();
+console.log(d3.getFullYear()); // exibe o ano
